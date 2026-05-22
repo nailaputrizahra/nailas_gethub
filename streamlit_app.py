aa4428b6bebaea868eb4a608d7d7344ec54d6afa
nailaputrizahra <nailaputrizahra0112@gmail.com>
2026-05-22 14:21:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,13 @@ st.write(
 import streamlit as st
 
 st.title("nayi's app")
-st.title("_Streamlit_ is :pink[shy]:3")
+st.title("_Streamlit_ is : pink[shy]:3")
+import streamlit as st
+
+st.header("_Streamlit_ is : pink[shy]:3")
+st.header("This is a header with a divider", divider="pink")
+st.header("These headers have rotating dividers", divider=True)
+st.header("One", divider=True)
+st.header("Two", divider=True)
+st.header("Three", divider=True)
+st.header("Four", divider=True)
