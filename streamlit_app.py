539c82c6be1720c3aa0b7147167a9e331fc439cb
nailaputrizahra <nailaputrizahra0112@gmail.com>
2026-05-22 14:22:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,9 @@ st.title("🎈 nayi's aplication")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-import streamlit as st
 
 st.title("nayi's app")
 st.title("_Streamlit_ is : pink[shy]:3")
-
-import streamlit as st
 
 st.header("_Streamlit_ is : pink[shy]:3")
 st.header("This is a header with a divider", divider="pink")
