be4ca9e648642f745a0fe02c2c2bebef82252de2
nailaputrizahra <nailaputrizahra0112@gmail.com>
2026-05-22 14:17:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title("🎈 nayi's aplication")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+st.title("This is a title")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
